b7d524de41bf6b0a28a66d39505dd644e31a1e1e
Rujie Yin <rujie.yin@duke.edu>
2016-09-20 09:45:49 -0400

    add loss and optimization

--- BinaryRegression.py
+++ BinaryRegression.py
@@ -28,9 +28,9 @@ class Weights:
                 # remove first dim for batch size
                 weight_shape = graph[key].get_shape()[1:]
                 self.weights[key+'_w'] = tf.Variable(tf.zeros(weight_shape), name = key+'_w')
-            self.shape = { self.weights[key].get_shape() for key in self.weights.keys() }
+            self.shape = { key: weight.get_shape() for key, weight in self.weights.iteritems() }
         else:
-            for key, s in self.shape.iteritems():
+            for key, s in shape.iteritems():
                 self.weights[key] = tf.Variable(tf.zeros(s), name = key)
             self.shape = shape
 
@@ -49,28 +49,40 @@ class Weights:
     def sqr_norm(self):
         return sum([tf.reduce_sum(tf.square(w)) for _, w in self.weights.iteritems()])
 
+    def compute_reg(self, graph):
+
+        def _inner_prod(t1, t2):
+            # use broadcast of tf.mul, reduce sum in consistant dim
+            return tf.reduce_sum(tf.mul(t1,t2), [1, 2, 3])
+
+        # sum of inner products of output coeffs and weights
+        return sum([_inner_prod(weight, graph[key[:-2]]) for key, weight in self.weights.iteritems() ])
+
+
 def build_graph(image):
     graph = load_vgg_model(VGG_MODEL, input_image = image)
     model_var = tf.all_variables()
-    # # weights for each layer of output
-    # weights = {}
-    # for key in graph.keys():
-    #     # remove first dim for batch size
-    #     weight_shape = graph[key].get_shape()[1:]
-    #     weights[key+'_w'] = tf.Variable(tf.random_normal(weight_shape), name = key+'_w')
 
-    def _inner_prod(t1, t2):
-        # use broadcast of tf.mul, keep the first batch dimension in sum
-        return tf.reduce_sum(tf.mul(t1,t2), [1, 2, 3])
+    def _normalize_graph(graph):
+        for key, val in graph.iteritems():
+            graph[key] = tf.scalar_mul(.1/tf.reduce_mean(val), val)
 
-    weights = Weights(graph = graph)
-    weights = weights.weights
+    _normalize_graph(graph)
 
-    # sum of inner products of output coeffs and weights
-    regs = sum([_inner_prod(weights[key], graph[key[:-2]]) for key in weights.keys() ])
+    return (graph, model_var)
 
-    return (regs, model_var, weights)
 
+def reg_loss(regs, labels):
+    return tf.reduce_mean(tf.squared_difference(regs , labels))
+
+def residual_loss(beta, z, u):
+    return beta.sub(z).add(u).sqr_norm()
+
+def start_session(model_var):
+    sess = tf.InteractiveSession()
+    sess.run(tf.initialize_variables(model_var))
+    sess.run(tf.initialize_all_variables())
+    return sess
 
 def binary_reg():
     # Content image to use.
@@ -79,18 +91,34 @@ def binary_reg():
     # Style image to use.
     STYLE_IMAGE = 'images/inputs/Nr2_original_p1-ds.jpg' #'images/inputs/Nr2_orig.jpg'
     style_image = load_image(STYLE_IMAGE, image_width=IMAGE_WIDTH, image_height=IMAGE_HEIGHT)
-    label = [0 , 1]
+    labels = tf.constant([0 , 1], dtype = 'float32')
 
-    regs, model_var, weights = build_graph(tf.concat(0, [content_image, style_image]))
+    graph, model_var = build_graph(tf.concat(0, [content_image, style_image]))
 
-    sess = tf.InteractiveSession()
-    sess.run(tf.initialize_variables(model_var))
-    sess.run(tf.initialize_all_variables())
+    beta = Weights(graph = graph)
+    regs = beta.compute_reg(graph)
 
-    print( "total number of weight variables: %.4e" % sess.run(sum([tf.size(v) for key, v in weights.iteritems()])) )
+    z = Weights(graph = graph)
+    u = Weights(graph = graph)
 
-    #print("%.4e" % (sess.run(tf.reduce_sum(graph['input']))) )
-    print(["%.4e" % reg for reg in sess.run(regs)])
+    loss = reg_loss(regs, labels) + residual_loss(beta, z, u)
+    opt = tf.train.AdamOptimizer(learning_rate=0.0000001)
+    opt_op = opt.minimize(loss, var_list=beta.weights.values())
+
+    sess = start_session(model_var)
+
+    print( "total number of weight variables: %.4e" % sess.run(sum([tf.size(v) for key, v in beta.weights.iteritems()])) )
+
+    loss_val = sess.run(loss)
+    itr = 0
+    while loss_val > 1.0e-2 :
+        print("iteration %d:" % itr)
+        opt_op.run()
+        print("loss: %.4e" % loss_val)
+        loss_val = sess.run(loss)
+        itr = itr + 1
+        # print( "reg_loss: %.4e" % sess.run(reg_loss(regs, labels)))
+        # print( "residual_loss: %.4e" % sess.run(residual_loss(beta, z, u)))
 
 if __name__ == '__main__':
     binary_reg()
